add9871e952cd235cdcca8fb3c0e31fb6fce49c4
Matvey <metrloopod2121@mail.ru>
2024-12-23 23:35:33 +0300

    Initial Commit

---
+++ collageMaker/collageMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  collageMakerApp.swift
+//  collageMaker
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct collageMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
